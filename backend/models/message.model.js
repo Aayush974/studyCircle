fcfbb0fb9c4f9d686535a3baf0988a9ff3ff6a25
Aayush <chandayush005@gmail.com>
2025-09-25 19:33:52 +0530

    add message model and route

---
+++ backend/models/message.model.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+    },
+    senderId: {
+      type: String,
+      required: true,
+    },
+    targetId: {
+      type: String,
+      required: true,
+    },
+    targetType: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      enum: ["workspace", "studyRoom", "user"],
+    },
+    attaachemnts: [
+      {
+        fileUrl: {
+          type: String,
+          required: true,
+        },
+        fileType: {
+          type: String,
+          required: true,
+          trim: true,
+          lowercase: true,
+          enum: ["pdf", "image"],
+        },
+        fileName: {
+          type: String,
+          required: true,
+        },
+        fileSize: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+messageSchema.index({
+    targetId:1,
+    targetType:1,
+    createdAt: -1
+})
+
+messageSchema.index({
+    senderId: 1
+})
+
+const Message = mongoose.model("Message", messageSchema);
+
+export { Message };
